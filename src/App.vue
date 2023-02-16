1647c814f1fd1ee1cb431009ea13fb90ca86bc50
chunzkok <kokcz@comp.nus.edu.sg>
2023-02-16 21:41:00 +0800

    Refactor HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 import { ref, onMounted } from "vue";
 import getRandPublicImgUrl from "./services/ArticApi";
 
@@ -26,8 +25,13 @@ onMounted(() => {
     </div>
 
     <div class="wrapper">
-      <HelloWorld msg="Welcome!" />
-
+      <div class="greetings">
+        <h1 class="green">Welcome!</h1>
+        <h3>
+          Please enjoy this wonderful piece of artwork, from the Art Institute
+          Chicago.
+        </h3>
+      </div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -39,6 +43,16 @@ onMounted(() => {
 </template>
 
 <style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
 header {
   display: flex;
   justify-content: center;
@@ -46,6 +60,10 @@ header {
   flex-direction: column;
 }
 
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
 .artwork {
   display: block;
   margin-left: auto;
@@ -82,6 +100,11 @@ nav a:first-of-type {
 }
 
 @media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: center;
+  }
+
   header {
     display: flex;
     place-items: center;
