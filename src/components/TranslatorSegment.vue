0e9ee007996bfb2384b9da9ade903dc0496a1a5c
chunzkok <kokcz@comp.nus.edu.sg>
2023-02-16 21:56:53 +0800

    Add minor styling to placeholder translate segment

--- src/components/TranslatorSegment.vue
+++ src/components/TranslatorSegment.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>
-        <h1>
-            WIP: LibreTranslate API integration
-        </h1>
+    <div class="placeholderText d-flex ">
+    <h1>WIP: LibreTranslate API integration</h1>
     </div>
 </template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .placeholderText {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    text-align: center;
+  }
+}
+</style>
