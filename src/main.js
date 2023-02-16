0bd3da14542d1290ee15d17f7a4e5a9489b5df6f
chunzkok <kokcz@comp.nus.edu.sg>
2023-02-16 13:06:30 +0800

    Integrate Bootstrap into project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "bootstrap";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/main.css";
 
 const app = createApp(App);
