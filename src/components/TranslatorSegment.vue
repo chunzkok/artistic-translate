a00b25763749f54e8f5b3c753e43bd359a50de23
chunzkok <kokcz@comp.nus.edu.sg>
2023-02-16 21:44:48 +0800

    Remove unnecessary components - TheWelcome

---
+++ src/components/TranslatorSegment.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>
+            WIP: LibreTranslate API integration
+        </h1>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+import TranslatorSegment from '../components/TranslatorSegment.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <TranslatorSegment />
   </main>
 </template>
